0388e7fe380d368995036d9c28df3ad36ca1ce2e
William Plaquet <wplaquet72@gmail.com>
2025-01-29 18:49:56 +0100

    Déploiement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,10 @@ export const Hero = () => {
                     <h1 className="text-6xl font-bold text-[#adbac7]">Développeur Web</h1>
                     <p className="mt-4 text-lg text-[#9fa6b2]">Développeur Full-Stack / Développeur Frameworks</p>
                     <div className="mt-4 flex">
-                        <button className="mr-6 py-2 px-6 font-medium text-[#a9b4be] border border-[#3a3d40] bg-[#23282f] rounded cursor-pointer duration-100 hover:bg-[#303740]">
+                        <button
+                            className="mr-6 py-2 px-6 font-medium text-[#a9b4be] border border-[#3a3d40] bg-[#23282f] rounded cursor-pointer duration-100 hover:bg-[#303740]"
+                            onClick={() => window.open('/CV.pdf', '_blank')}
+                            >
                             Télécharger le CV
                         </button>
                         <a href="https://www.linkedin.com/in/william-plaquet-9b6682210/" target="blank" className="flex justify-center items-center mr-6 p-2 font-medium text-[#a9b4be] border border-[#3a3d40] bg-[#23282f] rounded duration-100 hover:bg-[#303740]">
